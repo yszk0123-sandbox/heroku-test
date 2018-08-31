a142986245e1d6bf29f7d8db91014b577513c190
Yuji Suzuki <y0suzuki0123@gmail.com>
2018-08-31 19:58:37 +0900

    Add an example to use environment variable

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
+const NAME = process.env.MY_NAME || 'unknown';
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send(`Hello ${NAME}!`));
 
 app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
